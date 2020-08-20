cd33be12f4fd50f38bb26c5bf862045bd9eb2a0a
seth-way <seth@Sethaniels-MacBook-Pro.local>
2020-08-20 14:41:59 -0700

    remove 'designed by' link from footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 SWay</li>
            </ul>
 
         </div>
