7d789aa870d323a8ae6850c1f78d7880ae86011d
seth-way <seth@Sethaniels-MacBook-Pro.local>
2020-08-21 18:43:03 -0700

    update landing page background color to fix color issue

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,7 +18,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg color="#9cedff" num={200} type="cobweb" bg={true} />
+      <ParticlesBg color="#305496" num={200} type="cobweb" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
