1faef44bbe7fb94475d555815e6d8351978ae4ff
seth-way <seth@Sethaniels-MacBook-Pro.local>
2020-08-28 12:00:05 -0700

    add algorithms section to app page layout

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 // import Portfolio from './Components/Portfolio';
+import Algorithms from './Components/Algorithms';
 
 class App extends Component {
 
@@ -49,6 +50,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         {/*<Portfolio data={this.state.resumeData.portfolio}/>*/}
+        <Algorithms />
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
